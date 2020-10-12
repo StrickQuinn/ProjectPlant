4e655e760a73664a1120050f534a6e8a52d0f8df
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-10-12 09:06:32 -0400

    create startGame fxn

--- Script/app.js
+++ Script/app.js
@@ -1,31 +1,29 @@
 
 $ (() => {
-    //Changed hiding method for modal
-    $('.modal').hide();
-    //hide plant until game starts
-    $('.plant').hide();
-    // hide until they are needed
-    $('.incorrect').hide()
-    $('.correct').hide()
-    $('#next').hide()
-    $('#try').hide()
-    $('.plant-name').hide()
-    $('#home').hide()
+    //create function to reset things at the home screen
+    const startGame = (e) => {
+        $('.modal').hide();
+        $('.plant').hide();
+        $('.incorrect').hide()
+        $('.correct').hide()
+        $('#next').hide()
+        $('#try').hide()
+        $('.plant-name').hide()
+        $('#home').hide()
+        $('#start').show()
+        console.log('hi')
+    }
+    startGame();
     
-
     //Test red square clickability
     $('.plant').on('click', () => {
         $('#start').show();
     });
 
-    //When the start button is clicked, hide the start button, and show the welcome
+    //When the start button is clicked, bring Welcome screen in front of the container
     $('#start').on('click', () => {
         $(event.currentTarget).hide();
-        $('#welcome').show();
-        $('.container').css({'z-index': -1});
-        $('.plant-name').hide();
-        $('.plant').hide();
-        
+        $('#welcome').show().css({'z-index': 1});
     })
 
     //When the play button is clicked, hide welcome, and show level1
@@ -37,27 +35,7 @@ $ (() => {
     //When you click on restart button, return to start screen;
     $('#restart').on('click', () => {
         $('.modal').hide()
-        $('#start').show();
-    })
-
-    //When you click on answer A, show correct screen
-    $('#a').on('click', () => {
-        $('.correct').show()
-        $('.answer').hide();
-        $('#next').show()
-    })
-
-    //Click on try again and refresh screen with b hidden
-    $('#try').on('click', () => {
-        $('.incorrect').hide()
-        $('#b').hide();
-        $('#try').hide();
-    })
-
-    //When b is clicked show incorrect screen
-    $('#b').on('click', () => {
-        $('.incorrect').show();
-        $('#try').show();
+        startGame();
     })
 
     //When you click the close button the modal disappears
@@ -72,11 +50,11 @@ $ (() => {
         $('.plant').show();
     })
 
-    //When plant is clicked show correct answer//
-        $('.plant').on('click', () => {
-            $('.plant-name').show()
-            $('#start').hide()
-            $('#home').show();
+    //Click on try again and refresh screen with b hidden
+    $('#try').on('click', () => {
+        $('.incorrect').hide()
+        $('#b').hide();
+        $('#try').hide();
     })
 
     //When home is clicked, take back to start screen
@@ -86,4 +64,25 @@ $ (() => {
         $('#start').show();
         $('#home').hide();
     })
+
+    //When you click on answer A, show correct screen
+    $('#a').on('click', () => {
+        $('.correct').show()
+        $('.answer').hide();
+        $('#next').show()
+    })
+
+    //When b is clicked show incorrect screen
+    $('#b').on('click', () => {
+        $('.incorrect').show();
+        $('#try').show();
+    })
+
+    //When plant is clicked show correct answer//
+        $('.plant').on('click', () => {
+            $('.plant-name').show()
+            $('#start').hide()
+            $('#home').show();
+    })
+
 })
