e85aee9542caba6f351f5e9d13f52afab4d1e02c
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-10-11 21:51:17 -0400

    centered the game & cleaned up the clicking sequence

--- Script/app.js
+++ Script/app.js
@@ -2,24 +2,25 @@ $ (() => {
     //Changed hiding method for modal
     $('.modal').hide();
 
+    //hide plant until game starts
+    $('.plant').hide();
+
     //Test red square clickability
     $('.plant').on('click', () => {
         alert('plant');
     });
 
-    //Test close button clickability
-    // $('#close').on('click', () => {
-    //     alert('close');
-    // })
-
     //When the start button is clicked, hide the start button &show the modal class
     $('#start').on('click', () => {
         $(event.currentTarget).hide()
         $('.modal').show();
+        $('.container').css({'z-index': -1});
     })
 
     //When the close button is clicked, make the modal disappear
     $('#close').on('click', () => {
         $('.modal').hide();
+        //show plant when close is clicked
+        $('.plant').show();
     })
 })
