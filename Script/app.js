e0e3fe121e4d2eea8cd6a7e1ba62bee7e23903ac
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-10-11 21:24:39 -0400

    make start hide when clicked & make modal disappear when close is clicked

--- Script/app.js
+++ Script/app.js
@@ -2,23 +2,24 @@ $ (() => {
     //Changed hiding method for modal
     $('.modal').hide();
 
-    //Test start button clickability
-    $('#start').on('click', () => {
-        alert('start');
-    });
-
     //Test red square clickability
     $('.plant').on('click', () => {
         alert('plant');
     });
 
     //Test close button clickability
-    $('#close').on('click', () => {
-        alert('close');
+    // $('#close').on('click', () => {
+    //     alert('close');
+    // })
+
+    //When the start button is clicked, hide the start button &show the modal class
+    $('#start').on('click', () => {
+        $(event.currentTarget).hide()
+        $('.modal').show();
     })
 
-    //When the start button is clicked, show the modal class
-    $('#start').on('click', () => {
-        $('.modal').show();
+    //When the close button is clicked, make the modal disappear
+    $('#close').on('click', () => {
+        $('.modal').hide();
     })
 })
