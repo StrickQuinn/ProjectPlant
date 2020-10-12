99c6e63fc2ce2e401584c3ba8b68b4fab733c798
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-10-12 15:28:17 -0400

    added a picture & cleaned up onload screen

--- Script/app.js
+++ Script/app.js
@@ -1,50 +1,37 @@
+
+
 $ ((e) => {
+    //Keep screen from auto-refresh when button is clicked
+    $('button').on('click', (e) => {
+        e.preventDefault();
+    });
+
     //create function to reset things at the home screen
     const startGame = () => {
-        $('#start').show()
-        $('.modal').hide();
-        $('.plant').hide();
-        $('.incorrect').hide()
-        $('.correct').hide()
-        $('#next').hide()
-        $('#try').hide()
-        $('.plant-name').hide()
+    //     $('#start').show()
+    //     $('.modal').hide();
+    //     $('.plant').hide();
+    //     $('.incorrect').hide()
+    //     $('.correct').hide()
+    //     $('#next').hide()
+    //     $('#try').hide()
+    //     $('.plant-name').hide()
         $('#home').hide()
-        $('#start').show()
+    //     $('#start').show()
         $('.level').hide()
-        $('.answer').show();
+    //     $('.answer').show();
         console.log('hi')
     }
     startGame();
 
-    const goHome = () => {
-        $('.plant').hide()
-        $('.plant-name').hide();
-        $('#start').show();
-        $('#home').hide();
-        $('.answer').hide();
-        console.log('went home')
-    }
-    
-
-    //Keep screen from auto-refresh when button is clicked
-    $('button').on('click', (e) => {
-        e.preventDefault();
-    })
-
-    //Test red square clickability
-    $('.plant').on('click', () => {
-        $('#start').show();
-    });
-
     //When the start button is clicked, bring Welcome screen in front of the container
     $('#start').on('click', () => {
-        $('#start').hide();
+        $('.container').hide();
         $('#welcome').show().css({'z-index': 1});
         console.log('start')
-    })
+    });
 
-    //When the play button is clicked, hide welcome, and show level1
+        //When the play button is clicked, hide welcome, and show level1
     $('#play').on('click', () => {
         $('#welcome').hide();
         $('.level').show();
@@ -52,65 +39,91 @@ $ ((e) => {
         console.log('play');
     })
 
-    //When you click on restart button, return to start screen;
-    $('#restart').on('click', () => {
-        $('.level').hide();
-        goHome();
-        startGame();
-        console.log('hit restart button')
-    })
 
-    //When you click the close button the modal disappears
-    $('#close').on('click', () => {
-        $('.modal').hide()
-        $('#start').show()
-        console.log('close')
-    })
 
-    //When next button is clicked, show plant
-    $('#next').on('click', () => {
-        $('.level').hide();
-        $('.plant').show();
-        $('.answer').show();
-        console.log('next scene')
-    })
 
-    //Click on try again and refresh screen with b hidden
-    $('#try').on('click', () => {
-        $('.incorrect').hide()
-        $('#b').hide();
-        $('#try').hide();
-        console.log('try again')
-    })
 
-    //When home is clicked, take back to start screen
-    $('#home').on('click', () => {
-        $('.incorrect').hide()
-        $('.correct').hide()
-        goHome();
-        console.log('bye')
-    })
 
-    //When you click on answer A, show correct screen
-    $('#a').on('click', () => {
-        $('.correct').show()
-        $('.answer').hide();
-        $('#next').show()
-        console.log('correct answer')
-    })
+//     const goHome = () => {
+//         $('.plant').hide()
+//         $('.plant-name').hide();
+//         $('#start').show();
+//         $('#home').hide();
+//         $('.answer').hide();
+//         console.log('went home')
+//     }
+    
 
-    //When b is clicked show incorrect screen
-    $('#b').on('click', () => {
-        $('.incorrect').show();
-        $('#try').show();
-        console.log('wrong answer')
-    })
+//     
 
-    //When plant is clicked show correct answer//
-        $('.plant').on('click', () => {
-            $('.plant-name').show()
-            $('#start').hide()
-            $('#home').show();
-            console.log('plant')
-    })
+//     //make it an img
+//     $('img').on('click', () => {
+//         $('#start').show();
+//         console.log('gecko')
+//     });
+
+
+
+//     //When you click on restart button, return to start screen;
+//     $('#restart').on('click', () => {
+//         $('.level').hide();
+//         goHome();
+//         startGame();
+//         console.log('hit restart button')
+//     })
+
+//     //When you click the close button the modal disappears
+//     $('#close').on('click', () => {
+//         $('.modal').hide()
+//         $('#start').show()
+//         console.log('close')
+//     })
+
+//     //When next button is clicked, show plant
+//     $('#next').on('click', () => {
+//         $('.level').hide();
+//         $('.container').show();
+//         $('.answer').show();
+//         console.log('next scene')
+//     })
+
+//     //Click on try again and refresh screen with b hidden
+//     // $('#try').on('click', () => {
+//     //     $('.incorrect').hide()
+//     //     $('#b').hide();
+//     //     $('#try').hide();
+//     //     console.log('try again')
+//     // })
+
+//     //When home is clicked, take back to start screen
+//     $('#home').on('click', () => {
+//         $('.incorrect').hide()
+//         $('.correct').hide()
+//         goHome();
+//         console.log('bye')
+//     })
+
+//     //When you click on answer A, show correct screen
+//     $('#a').on('click', () => {
+//         $('.correct').show()
+//         $('.answer').hide();
+//         $('#next').show()
+//         console.log('correct answer')
+//     })
+
+//     //When b is clicked show incorrect screen
+//     $('#b').on('click', () => {
+//         $('.incorrect').show();
+//         $('#try').show();
+//         console.log('wrong answer')
+//     })
+
+//     //When plant is clicked show correct answer//
+//         $('.plant').on('click', () => {
+//             $('.plant-name').show()
+//             $('#start').hide()
+//             $('#home').show();
+//             console.log('plant')
+//     })
 })
+
