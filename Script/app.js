7ff66dd962bcbdacd307d997915955b0c18d9f7c
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-10-11 20:00:54 -0400

    red square  is clickable

--- Script/app.js
+++ Script/app.js
@@ -1,6 +1,12 @@
 $ (() => {
-    //Test button click
+
+    //Test start button clickability
     $('#start').on('click', () => {
         alert('start');
-    })
+    });
+
+    //Test req square clickability
+    $('.plant').on('click', () => {
+        alert('plant')
+    });
 })
