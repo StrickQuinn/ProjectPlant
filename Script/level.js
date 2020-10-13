bf5e3ec5b08991bbcd93ae0177794c8f17824cac
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-10-13 09:55:11 -0400

    added new js file & gave modal game goals

---
+++ Script/level.js
@@ -0,0 +1,11 @@
+console.log('Hey you')
+$ (() => {
+    const $answer = $('.answer')
+    const $a = $('#a')
+    const $b = $('#b')
+    const $c = $('#c')
+    const $d = $('#d')
+    $answer.on('click', () => {
+        
+    })
+})
