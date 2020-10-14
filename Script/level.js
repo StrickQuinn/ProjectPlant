4a67869917c9ffcd9425fcd3d817c86e78fa5e94
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-10-14 15:23:33 -0400

    cleared level.js  & added checkAnswer fxn

--- Script/level.js
+++ Script/level.js
@@ -6,13 +6,15 @@ $ (() => {
         e.preventDefault();
     });
 
-//give player options to choose    
-    let $a = $('#a').text()
-    let $b = $('#b').text()
-    let $c = $('#c').text()
-    let $d = $('#d').text()
-    playerAnswer = [$a, $b, $c, $d]
-    console.log(playerAnswer[1])
+
+//create a fxn to check for the correct answer
+const checkAnswer = () => {
+    if ($('.playerAnswer') == $('.correctAnswer')) {
+        console.log('This is the correct answer')
+    }else {
+        console.log('This is the wrong answer')
+    }
+}
 
 
 
@@ -25,40 +27,6 @@ $ (() => {
 
 
 
-    /////Will use a version of this to check player's choice
 
-    // let correctChoice = $a.text()
-    // const choice = () => {
-    //     $a.on('click', () => {
-    //         if ($a.text() == correctChoice) {
-    //             $answer.text(correctChoice + ' is the correct!')
-    //             console.log($answer.text())
-    //         }else {
-    //             $answer.text(correctChoice + ' is the no!')
-    //             console.log($answer.text() + ' is incorrect')
-    //         }
-    //     })
-    //     $b.on('click', () => {
-    //         if ($b.text() == correctChoice) {
-    //             console.log($b.text() + ' is correct')
-    //         }else {
-    //             console.log($b.text() + ' is incorrect')
-    //         }
-    //     })
-    //     $c.on('click', () => {
-    //         if ($c.text() == correctChoice) {
-    //             console.log($c.text() + ' is correct')
-    //         }else {
-    //             console.log($c.text() + ' is incorrect')
-    //         }
-    //     })
-    //     $d.on('click', () => {
-    //         if ($d.text() == correctChoice) {
-    //             console.log($d.text() + ' is correct')
-    //         }else {
-    //             console.log($d.text() + ' is incorrect')
-    //         }
-    //     })
-    // }
-    // choice()
+
 })
