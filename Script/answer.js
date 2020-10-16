9aee214ef7bc80f2ea95bb27f7f1678d4175f51c
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-10-16 04:04:25 -0400

    level round results are correct

--- Script/answer.js
+++ Script/answer.js
@@ -14,66 +14,65 @@ $(() => {
     //hide choice buttons
     //create a playGame fxn to wrap whether or not the answer is correct
     //when the button is clicked, remove all buttons
-    const playerAnswer = () => {
-        let $a = $('#a').text()
-        let $b = $('#b').text()
-        let $c = $('#c').text()
-        let $d = $('#d').text()
-        let $correct = $('.correctAnswer').text()
-        let $result = $('.result')
+    // const playerAnswer = () => {
+    //     let $a = $('#a').text()
+    //     let $b = $('#b').text()
+    //     let $c = $('#c').text()
+    //     let $d = $('#d').text()
+    //     let $correct = $('.correctAnswer').text()
+    //     let $result = $('.result')
     
-        $('#a').on('click', () => {
-            console.log($a)
-            $('.playerChoice').hide()
-            $('.continue').show()
-            if ($a === $correct) {
-                $result.text('That is correct!')
+    //     $('#a').on('click', () => {
+    //         console.log($a)
+            
+    //         if ($a === $correct) {
+    //             $result.text('That is correct!')
                 
-                totalDollars()
-            }else {
-                $result.text('That is incorrect. The correct answer is ' + $correct + '.')
-            }
-            $('.result').show()
+    //             totalDollars()
+    //         }else {
+    //             $result.text('That is incorrect. The correct answer is ' + $correct + '.')
+    //         }
+    //         $('.result').show()
 
-        });
-        $('#b').on('click', () => {
-            console.log($b)
-            $('.continue').show()
-            $('.playerChoice').hide()
-            if ($b === $correct) {
-                $result.text('That is correct!')
-                totalDollars()
-            }else {
-                $result.text('That is incorrect. The correct answer is ' + $correct + '.')
-            }
-            $('.result').show()
-        });
+    //     });
+    //     $('#b').on('click', () => {
+    //         console.log($b)
+    //         $('.continue').show()
+    //         $('.playerChoice').hide()
+    //         if ($b === $correct) {
+    //             $result.text('That is correct!')
+    //             totalDollars()
+    //         }else {
+    //             $result.text('That is incorrect. The correct answer is ' + $correct + '.')
+    //         }
+    //         $('.result').show()
+    //     });
         
-        $('#c').on('click', () => {
-            console.log($c)
-            $('.continue').show()
-            $('.playerChoice').hide()
-            if ($c === $correct) {
-                $result.text('That is correct!')
-                totalDollars()
-            }else {
-                $result.text('That is incorrect. The correct answer is ' + $correct + '.')
-            }
-            $('.result').show()
-        });
-        $('#d').on('click', () => {
-            console.log($d)
-            $('.continue').show()
-            $('.playerChoice').hide()
-            if ($d === $correct) {
-                $result.text('That is correct!')
-                totalDollars()
-            }else {
-                $result.text('That is incorrect. The correct answer is ' + $correct + '.')
-            }
-            $('.result').show()
-        })
-    }
+    //     $('#c').on('click', () => {
+    //         console.log($c)
+    //         $('.continue').show()
+    //         $('.playerChoice').hide()
+    //         if ($c === $correct) {
+    //             $result.text('That is correct!')
+    //             totalDollars()
+    //         }else {
+    //             $result.text('That is incorrect. The correct answer is ' + $correct + '.')
+    //         }
+    //         $('.result').show()
+    //     });
+    //     $('#d').on('click', () => {
+    //         console.log($d)
+    //         $('.continue').show()
+    //         $('.playerChoice').hide()
+    //         if ($d === $correct) {
+    //             $result.text('That is correct!')
+    //             totalDollars()
+    //         }else {
+    //             $result.text('That is incorrect. The correct answer is ' + $correct + '.')
+    //         }
+    //         $('.result').show()
+    //     })
+    // }
 
     //create a way to find the current level
     //create a way to change the level based on current level
--- Script/app.js
+++ Script/app.js
@@ -44,10 +44,99 @@ $(() => {
     })
 
 
+    //Determine current level, before collecting response
+    const currentLevel = () => {
+        if (totalLevels[0]) {
+            $a = $('#a0')
+            $b = $('#b0')
+            $c = $('#c0')
+            $d = $('#d0')
+            $correct = $('.correctAnswer0')
+            console.log($a.text())
+            console.log($correct.text())
+        }
+    }
+
+    const playerAnswer = () => {
+        let $result = $('.result');
+        currentLevel();
+        $correct.hide()
+        if ($a.on('click', () => {
+            $('.playerChoice').hide()
+            $('.continue').show()
+            if ($a.text() === $correct.text()) {
+                $result.text('That is correct!')
+                console.log('yay')
+                totalDollars()
+            }else {
+                $result.text('That is incorrect. The correct answer is ' + $correct.text() + '.')
+                console.log('oh no')
+            }
+            $('.result').show()
+        }))
+        if ($b.on('click', () => {
+            $('.playerChoice').hide()
+            $('.continue').show()
+            if ($b.text() === $correct.text()) {
+                $result.text('That is correct!')
+                console.log('yay')
+                totalDollars()
+            }else {
+                $result.text('That is incorrect. The correct answer is ' + $correct.text() + '.')
+                console.log('oh no')
+            }
+            $('.result').show()
+        }))
+        if ($c.on('click', () => {
+            $('.playerChoice').hide()
+            $('.continue').show()
+            if ($c.text() === $correct.text()) {
+            $result.text('That is correct!')
+                
+                totalDollars()
+            }else {
+                $result.text('That is incorrect. The correct answer is ' + $correct.text() + '.')
+            }
+            $('.result').show()
+        }))
+        ($d.on('click', () => {
+            $('.playerChoice').hide()
+            $('.continue').show()
+            if ($d.text() === $correct.text()) {
+            $result.text('That is correct!')
+                
+                totalDollars()
+            }else {
+                $result.text('That is incorrect. The correct answer is ' + $correct.text() + '.')
+            }
+            $('.result').show()
+        }))
+    }
+    playerAnswer()
 
 
 
-    //Clicke continue and have plant-name, img and next btn show
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Click continue and have plant-name, img and next btn show
     $('.continue').on('click', () => {
         $('.plant-name').show()
         $('img').show()
